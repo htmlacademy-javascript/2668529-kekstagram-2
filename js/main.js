3b863164b4d7961c1983f6c9c36008aaf8fbfda5
chaykinks <chaykinks@gmail.com>
2025-10-07 18:46:54 +0500

    вынес данные и генераторы в модули data и util

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-const NAMES = [
-  'Светлана',
-  'Николай',
-  'Джон',
-  'Мария',
-  'Дмитрий',
-  'Иван',
-  'Александр',
-  'Петр',
-  'Кирилл',
-  'Василий'
-];
-
-const DESCRIPTION = [
-  'Отличное фото!',
-  'Фото не очень',
-  'Фото не очень снова)',
-  'Описание фото 4',
-  'Описание фото 5',
-  'еще одно описание фото'
-];
-
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
 const MAX_PHOTO_ID = 25;
 const MAX_PHOTO_NUMBER = 25;
 const MIN_LIKES = 15;
@@ -38,30 +7,11 @@ const MAX_AVATAR_NUMBER = 6;
 const MAX_COMMENTS = 30;
 const PHOTO_ARRAY_COUNT = 25;
 
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
+
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-function createRandomId(min, max) {
-  const previousValues = [];
 
-  return function() {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-}
 
 const generatePhotoId = createRandomId(1, MAX_PHOTO_ID);
 const generateCommentId = createRandomId(1, MAX_COMMENT_ID);
