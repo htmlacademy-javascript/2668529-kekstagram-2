8c5c739cfcd1928b22078e247c7da95d77f57d57
chaykinks <chaykinks@gmail.com>
2025-10-30 17:30:54 +0500

    реализовал фильтры миниатюр

--- js/pictures.js
+++ js/pictures.js
@@ -3,8 +3,17 @@ import { getData } from './api.js';
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 const templateDataError = document.querySelector('#data-error').content.querySelector('.data-error');
+const filters = document.querySelector('.img-filters');
+const filterButton = filters.querySelectorAll('.img-filters__button');
+
+const RANDOM_PICTURES_COUNT = 10;
+const RERENDER_DELAY = 500;
 
 let allPictures = [];
+
+const clearPictures = () => {
+  pictures.querySelectorAll('.picture').forEach((element) => element.remove());
+};
 
 const renderPictures = (picturesList) => {
   const picturesFragment = document.createDocumentFragment();
@@ -26,10 +35,58 @@ const showDataError = () => {
   setTimeout(() => dataErrorMessage.remove(), 5000);
 };
 
+const getRandomUnique = (array, count) => {
+  const shuffled = [...array].sort(() => Math.random() - 0.5);
+  return shuffled.slice(0, count);
+};
+
+const sortByComments = (a, b) => b.comments.length - a.comments.length;
+
+const debounce = (callback, delay = RERENDER_DELAY) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callback(...args), delay);
+  };
+};
+
+const updatePictures = (filterId) => {
+  clearPictures();
+  let filteredPictures = [];
+
+  switch (filterId) {
+    case 'filter-random':
+      filteredPictures = getRandomUnique(allPictures, RANDOM_PICTURES_COUNT);
+      break;
+    case 'filter-discussed':
+      filteredPictures = [...allPictures].sort(sortByComments);
+      break;
+    default:
+      filteredPictures = allPictures;
+  }
+
+  renderPictures(filteredPictures);
+};
+
+const setFilterHandlers = () => {
+  const debouncedUpdate = debounce(updatePictures);
+  filters.addEventListener('click', (evt) => {
+    const button = evt.target.closest('.img-filters__button');
+    if (!button) {
+      return;
+    }
+    filterButton.forEach((btn) => btn.classList.remove('img-filters__button--active'));
+    button.classList.add('img-filters__button--active');
+    debouncedUpdate(button.id);
+  });
+};
+
 const initPictures = async () => {
   try {
     allPictures = await getData();
     renderPictures(allPictures);
+    filters.classList.remove('img-filters--inactive');
+    setFilterHandlers();
   } catch (error) {
     showDataError();
   }
