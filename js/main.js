aaaa01a0e1f9fff933c8ffe5cf69541e2b081594
chaykinks <chaykinks@gmail.com>
2025-11-01 10:27:39 +0500

    добавил отображение выбранного файла в форме загрузки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { initFullSizePicture } from './fullsizePicture.js';
 import { initPictures } from './pictures.js';
 import './upload-image-form.js';
+import './preview.js';
 
 await initPictures();
 initFullSizePicture();
